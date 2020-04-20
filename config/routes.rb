eb79c5d22de33e116c852c4fdaa01ebf2cbcaa7f
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-20 07:00:50 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Add your routes here
-  get '/auth/:developer/callback', to: 'sessions#create', via: [:get, :post]
+  get '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 
   root 'welcome#home'
 end
